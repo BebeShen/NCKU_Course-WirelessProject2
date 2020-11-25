bc5c2208e49dc4ff058c12fcaa5f344dd1560227
BebeShen <OBebeShenO@gmail.com>
2020-11-25 23:37:10 +0800

    [feature] Complete `cars' generate`.
    - Use struct pos to implement `bases` and `entry` instead of class.
    - Complete `bases` and `entry` initialize.
    - Add Description of Program
    - Change vector<pair<int,pos> > to vector<pos>.
    
    TODO:
    
    - remove cars that arrive out port
    - `cars' move()`
    - Policy implement.

--- p2.cpp
+++ p2.cpp
@@ -1,7 +1,17 @@
+/**
+ * Author：@bebeshen
+ * Program Language：c++
+ * Description：
+ * Compile Command：`g++ p2.cpp -O3 -o p2.exe`
+ * Run Command：`./p2.exe`
+ * 
+ * 
+ **/ 
 #include<iostream>
 #include<vector>
 #include<cmath>
 #include<ctime>
+#include<fstream>
 using namespace std;
 #define SystemTime 86400
 class car
@@ -13,17 +23,11 @@ class car
         double x;
         double y;
 };
-class base
+typedef struct Pos
 {
-    public:
-        base();
-        base(double bx,double by);
-        double x;
-        double y;
-        double power;
-        //int getP();
-        double calGain(double x,double y);
-};
+    double x;
+    double y;
+}pos;
 car::car(double carx,double cary)
 {
     x = carx;
@@ -47,14 +51,7 @@ car::car()  // not use
     y = 0;
     direction = 0;
 }
-base::base(){
-    x=0;y=0;
-}
-base::base(double bx,double by){
-    x = bx;
-    y = by;
-}
-double calGain(base base,car car){
+double calGain(pos base,car car){
     double d = sqrt(pow(abs(car.x-base.x),2)+pow(abs(car.y - base.y),2));
     double pathLoss = 32.45 + 20*log10(d);
     return 100.0 - pathLoss;
@@ -63,33 +60,60 @@ double calGain(base base,car car){
 double lambda[3] = {1.0/2.0,1.0/3.0,1.0/5.0};
 
 int main(){
-    int entry_x[9] = {100,200,300,400,500,600,700,800,900}; // y = 0 or 1000
-    int entry_y[9] = {100,200,300,400,500,600,700,800,900}; // x = 0 or 1000
-    vector<car*>cars;
+    // initialize entry
+    vector<pos>entry;
+    entry.resize(36);
+    for (int i = 0; i < 9; ++i)
+    {
+        entry[i].x = 0.0;
+        entry[i].y = ((double)i+1.0)*100.0;
+        entry[i+9].x = 1000.0;
+        entry[i+9].y = ((double)i+1.0)*100.0;
+        entry[i+18].x = ((double)i+1.0)*100.0;
+        entry[i+18].y = 0.0;
+        entry[i+27].x = ((double)i+1.0)*100.0;
+        entry[i+27].y = 1000.0;
+    }
+    // initialize bases station
+    vector<pos>bases;
+    bases.resize(4);
+    bases[0].x = 330.0;    bases[0].y = 350.0;
+    bases[1].x = 640.0;    bases[1].y = 310.0;
+    bases[2].x = 360.0;    bases[2].y = 680.0;
+    bases[3].x = 660.0;    bases[3].y = 658.0;
+    // Do Policy
+    vector<car>cars;
     int num_car = 0;
     for(int i=0;i<3;++i){
-        for(int j=0;j<SystemTime;++j){
-            // remove cars that arrive out port : free()
-            // generate cars: malloc, realloc 
+        cout<<"++++++++++++++++++++++++++\nlambda:"<<lambda[i]<<"\n";
+        for(int j=0;j<3;++j){
+            cout<<"Second:"<<j<<"\n";
+            // cars' move
+            // remove cars that arrive out port
+            // generate cars
             srand (time(NULL));
             int numberGeneratedCar = 0;
-            for(int k=0;k<1000;++k){
+            for(int k=0;k<20;++k){
                 double prob = (double)rand() / RAND_MAX; // 0~1
-                cout<<prob<<endl;
+                //cout<<prob<<endl;
                 if(prob<=lambda[i]){
                     ++numberGeneratedCar;
                     ++num_car;
                 }
             }
-            cars.resize(num_car);
+            if(num_car>cars.size()){
+                cars.reserve(num_car);
+            }
             for(int k=0;k<numberGeneratedCar;++k){
-                car* tmp = (car*)malloc(sizeof(car));
-                int xORy = rand()%(1-0+1); 
-                int entryPoint = rand() % (3 - 0 + 1);
-                cars[num_car];
+                // Generate rand formula：int x = rand() % (max - min + 1) + min, min <= x <= max;
+                int entrypoint = rand()%(35 - 0 + 1) + 0;
+                cout<<"Random"<<entrypoint<<"\n";
+                car tmp(entry[entrypoint].x,entry[entrypoint].y);
+                cars.push_back(tmp);
             }
         }
-        cout<<"lambda:"<<lambda[i]<<endl;
+        cout<<"++++++++++++++++++++++++++\n";
     }
+    cars.clear();
     return 0;
 }
